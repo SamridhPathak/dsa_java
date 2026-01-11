dac3c026875fca9733a656f35b6e7d745a270f2c
Samridh Pathak <samridhpathak007@gmail.com>
2026-01-11 22:20:23 +0530

    new tree Q

---
+++ 09_trees/16_invertBT.java
@@ -0,0 +1,20 @@
+// LeetCode 226 - Invert Binary Tree
+// Difficulty: Easy
+// Approach: DFS Recursion, Traverse the tree using DFS and swap the left and right child of every node to mirror the tree.
+// Time Complexity: O(n)
+// Space Complexity: O(k)
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
