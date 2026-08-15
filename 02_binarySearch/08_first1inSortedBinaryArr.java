4e8215e3b141ebbb685801efde2b034dd8304f4d
Samridh Pathak <samridhpathak007@gmail.com>
2026-08-15 19:44:40 +0530

    edit in ans

--- 02_binarySearch/08_first1inSortedBinaryArr.java
+++ 02_binarySearch/08_first1inSortedBinaryArr.java
@@ -2,3 +2,23 @@
 // Difficulty: Easy
 // Approach: Find the first occurrence of 1 using binary search. If arr[mid] == 1, store mid as ans and continue searching left. If arr[mid] == 0, search right. Return ans after the loop.
 // Space Complexity: O(1)
+
+class Solution {
+    public int firstIndex(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (arr[mid] == 1) {
+                ans = mid;
+                end = mid - 1;
+            } else if (arr[mid] < 1) {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
